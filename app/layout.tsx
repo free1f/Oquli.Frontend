12f4719c61d6dc768ac59071ef17d659f1931cbe
freelf <liseth.yggdrasil@gmail.com>
2024-02-12 15:42:33 -0400

    CU-86azajfcv_Front-LoginSign-up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next"
-import { Inter } from "next/font/google"
 import App from "@/app/(infrastructure)/_components/App/App"
 import Header from "@/app/(infrastructure)/_components/Header/Header"
-
-const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Oquli"
@@ -16,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body 
-        className={inter.className}
-      >
+      <body>
         <App>
           <Header />
           {children}  
